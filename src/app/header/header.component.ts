bdb83e67fad6b8e5d54f24a7646f16d45abd8ecc
kom4 <damir.komic@gmail.com>
2019-01-08 14:39:15 +0100

    0801 posao 2nd

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: '<app-header>',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class HeaderComponent implements OnInit {
+
+  section = '';
+  @Output() sectionEmitter = new EventEmitter<string>();
+
+  sectionShowHandler(section: string) {
+    this.section = section;
+    this.sectionEmitter.emit(this.section);
+  }
 
     constructor() {}
 
