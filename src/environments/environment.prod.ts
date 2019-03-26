ed2a31f03c117e09f83d64143b0f74ceb3eeaa8e
kom4 <damir.komic@gmail.com>
2019-03-26 13:52:45 +0100

    dsda

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD_gm5mjCOgaa_30ifFzWHE3gYZb0W2s5k',
+    authDomain: 'recipeappprod.firebaseapp.com',
+    databaseURL: 'https://recipeappprod.firebaseio.com',
+    projectId: 'recipeappprod',
+    storageBucket: 'recipeappprod.appspot.com',
+    messagingSenderId: '103691906843'
+  }
+};
